c7881c4ac3bb401c7dbe68f7ef80cb3cbd20e72f
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-24 21:59:42 -0300

    first commit

---
+++ first-sprint-main/Integrantes/README.md
@@ -0,0 +1,15 @@
+# first-sprint
+
+Henrique Luis Baesa - RM558977
+
+Jonatha Rasne - RM558437
+
+Leonardo Salazar - RM557484
+
+Trabalho de Front-end do Challenge
+
+Acesso Figma (Conta Google):
+
+jfiap2024@gmail.com
+
+senhaJFiAp
---
+++ first-sprint-main/first-sprint-main/$(document).js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
+;$(document).ready(function() {
+    $('.banner').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    });
+});
